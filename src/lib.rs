ea673e6d1ec539b4862111f2cce686b0633c4e6d
ChenemiKen <chenemiken15@gmail.com>
2025-06-19 19:57:08 +0100

    format and lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
     let transaction_bytes_res = hex::decode(raw_tx_hex);
-    if transaction_bytes_res.is_err() {return Err(String::from("Hex decode error"));}
+    if transaction_bytes_res.is_err() {
+        return Err(String::from("Hex decode error"));
+    }
     let transaction_bytes = transaction_bytes_res.unwrap();
-    if transaction_bytes.len() < 4 {return Err(String::from("Transaction data too short"))}
+    if transaction_bytes.len() < 4 {
+        return Err(String::from("Transaction data too short"));
+    }
     let version_bytes = (&transaction_bytes[0..4]).try_into().unwrap();
     let version = u32::from_le_bytes(version_bytes);
     Ok(version)
